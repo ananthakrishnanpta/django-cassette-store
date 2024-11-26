797cdd4eb8e593ed2eb5d30e26ac2a8246827cfb
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2024-11-26 17:35:32 +0530

    code complete until orders app. Payment app remaining

--- mySite/orders/models.py
+++ mySite/orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from cart.models import CartItem
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=[('PENDING','Pending'),('COMPLETED','Completed')])
+    razorpay_order_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Order #{self.id} for {self.user.username}"
+    
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order, related_name="order_details", on_delete=models.CASCADE)
+    cart_item = models.ForeignKey(CartItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Detail for order {self.order.id} - Product: {self.cart_item.product.name}"
+    
+    
--- mySite/orders/views.py
+++ mySite/orders/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from .models import Order, OrderDetails
+from cart.models import CartItem
 # Create your views here.
+
+
+def create_order(request):
+    cart_items = CartItem.objects.filter(user = request.user)
+    total_amount = sum(float(item.product.price) * item.quantity for item in cart_items)
+
+    order = Order.objects.create(
+        user = request.user,
+        total_amount = total_amount,
+        status = 'PENDING'
+        )
+    
+    for item in cart_items:
+        OrderDetails.objects.create(
+            order=order,
+            cart_item = item,
+            quantity = item.quantity,
+            price = item.product.price
+        )
+        item.delete()
+
+    return redirect('payment:create_razorypay_order', order_id = order.id)
